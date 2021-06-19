0d1745078edb323f563112acf08d2e496d702bf1
Yukilwc <misaka1033@gmail.com>
2021-06-19 21:33:57 +0800

    u

--- font-convert-min/gulpfile.js
+++ font-convert-min/gulpfile.js
@@ -1,4 +1,5 @@
 /*
+
  * @Author: 李文超
  * @Date: 2020-10-15 17:39:13
  * @LastEditors: 李文超
@@ -6,18 +7,37 @@
  * @Description: file content
  * @FilePath: \danyou\gulpfile.js
  */
+require('util').inspect.defaultOptions.depth = null
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename')
 var watcher = require('gulp-watch')
 var changed = require('gulp-changed');
 const concat = require('gulp-concat');
-const template = require('gulp-template')
-gulp.task('default', gulp.series(minFunc))
+const template = require('gulp-template');
+const srcName = 'PingFang.ttf'
+gulp.task('default', minFunc)
+// 获取命令行参数
+const getArgOptions = () => {
+    var minimist = require('minimist');
+    // var knownOptions = {
+    //     string: 'env',
+    //     default: { env: process.env.NODE_ENV || 'production' }
+    // };
+
+    var options = minimist(process.argv.slice(2));
+    console.log('==========options', process.argv, options)
+
+}
 // https://www.npmjs.com/package/gulp-fontmin
 function minFunc() {
     console.log('==========最小化字体',)
-    return gulp.src('')
-        .pipe()
-        .pipe(gulp.dest('./dist'))
+    let argOptions = getArgOptions()
+    console.log('==========',)
+    return gulp.src(`./src/fonts/${srcName}`)
+        .pipe(rename((path) => {
+            console.log('==========path', path)
+            path.basename += '-min'
+        }))
+        .pipe(gulp.dest('./dist/'))
 }
