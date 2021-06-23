551fa53fe81952374e938a2ac6c725c900df714e
liwenchao <misaka1033@gmail.com>
2021-06-23 13:41:39 +0800

    'u'

--- font-convert-min/streamTask.js
+++ font-convert-min/streamTask.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: 李文超
+ * @Date: 2021-06-23 10:08:05
+ * @LastEditors: 李文超
+ * @LastEditTime: 2021-06-23 13:24:16
+ * @Description: file content
+ * @FilePath: \font-convert-min\streamTask.js
+ */
 require('util').inspect.defaultOptions.depth = null
 var gulp = require('gulp');
 var rename = require('gulp-rename')
@@ -5,6 +13,7 @@ var changed = require('gulp-changed');
 var fontmin = require('gulp-fontmin-woff2')
 var font2css = require('gulp-font2css-display').default;
 const { DEFAULT_MIN_VERSION } = require('tls');
+const path = require('path')
 
 // 获取命令行参数
 const getArgOptions = () => {
@@ -16,19 +25,32 @@ const getArgOptions = () => {
 const streamTask = (cb) => {
     console.log('=========start steam task',)
     const buffers = []
-    gulp.src(['../../temp-work/pages/main/**/*.vue', "../../temp-work/pages/joinUs/**/*.vue"])
+    // D:\workspace\project\web\danyou-web-ssr
+    const basePath = 'D:/workspace/project/web/danyou-web-ssr'
+    gulp.src([
+        `${basePath}/pages/**/*.vue`,
+        `${basePath}/pages/**/*.js`,
+        `${basePath}/pages/**/*.json`,
+        `${basePath}/components/**/*.vue`,
+        `${basePath}/components/**/*.js`,
+        `${basePath}/components/**/*.json`,
+        `${basePath}/layouts/**/*.vue`,
+        `${basePath}/i18n/**/*.js`,
+    ])
         .on('data', function (file) {
             buffers.push(file.contents)
         })
         .on('end', function () {
             let text = Buffer.concat(buffers).toString('utf-8')
             text = [...new Set(text)].join('')
+            text = filterCn(text)
             getMin(text, cb)
         })
 
 }
 const getMin = (text, cb) => {
     console.log('==========text', text)
+    console.log('==========text length', text.length)
     let argOptions = getArgOptions()
     gulp.src(`./src/fonts/${argOptions.font}`)
         .pipe(fontmin({
@@ -54,7 +76,7 @@ const generateBase64 = (cb) => {
         })
 }
 const filterCn = (text) => {
-    return text
+    return text.replace(/[^\u4E00-\u9FA5]/g, '')
 }
 module.exports = {
     streamTask
--- font-convert-min/tools.js
+++ font-convert-min/tools.js
@@ -32,28 +32,7 @@ module.exports = {
 }
 
 
-// function minifyFont(text, cb) {
-//     gulp
-//         .src('src/font/*.ttf')
-//         .pipe(fontmin({
-//             text: text
-//         }))
-//         .pipe(gulp.dest('dest/font'))
-//         .on('end', cb);
-// }
 
-// gulp.task('fonts', function(cb) {
-
-//     var buffers = [];
-
-//     gulp
-//         .src('index.html')
-//         .on('data', function(file) {
-//             buffers.push(file.contents);
-//         })
-//         .on('end', function() {
-//             var text = Buffer.concat(buffers).toString('utf-8');
-//             minifyFont(text, cb);
-//         });
-
-// });
+const path = require('path')
+let pathStr = 'D:\workspace\project\web\danyou-web-ssr'
+console.log('==========',path.relative('.',pathStr))
