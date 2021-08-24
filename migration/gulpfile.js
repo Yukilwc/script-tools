2da1a22612f009cb1fa16416cfe3267c4f99f8bc
liwenchao <misaka1033@gmail.com>
2021-08-24 20:04:09 +0800

    u

--- migration/gulpfile.js
+++ migration/gulpfile.js
@@ -2,7 +2,7 @@
  * @Author: 李文超
  * @Date: 2020-10-15 17:39:13
  * @LastEditors: 李文超
- * @LastEditTime: 2021-08-24 19:52:21
+ * @LastEditTime: 2021-08-24 20:02:59
  * @Description: file content
  * @FilePath: \script-tools\elementui-color-replace\gulpfile.js
  */
@@ -27,9 +27,8 @@ const
 // gulp.task('default', gulp.series(function () {
 //     replaceColor()
 // }))
-// 从小程序工程，备份首页到备份文件夹，备份目标为配置的indexName
-// 从备份项目，导入指定首页到小程序，导入目标为配置的indexName
 
+// 从小程序工程，备份首页到备份文件夹，备份目标为配置的indexName
 function mp2Backup() {
     // 清空目标文件夹
     function handleClean() {
@@ -63,7 +62,43 @@ function mp2Backup() {
                 gulp.dest(`${mpBackupFolderPath}/images/${indexName}/`)
             )
     }
-    return gulp.series([handleClean,gulp.parallel([handleFile,handleImage])])
+    return gulp.series([handleClean, gulp.parallel([handleFile, handleImage])])
 }
-gulp.task('backup2Mp', () => { })
+// 从备份项目，导入指定首页到小程序，导入目标为配置的indexName
+function backup2Mp() {
+    // 清空目标文件夹
+    function handleClean() {
+        return gulp.src([`${mpFolderPath}/pages/index`, `${mpFolderPath}/images/${indexName}`], { read: false, allowEmpty: true, })
+            .pipe(rename((path) => {
+                console.log('==========rename path', path)
+            }))
+            .pipe(clean(
+                {
+                    force: true
+                }
+            ))
+    }
+    // 迁移文件
+    function handleFile() {
+        return gulp.src([`${mpBackupFolderPath}/pages/${indexName}/*`])
+            .pipe(rename((path) => {
+                console.log('==========rename path', path)
+            }))
+            .pipe(
+                gulp.dest(`${mpFolderPath}/pages/index/`)
+            )
+    }
+    // 迁移图片
+    function handleImage() {
+        return gulp.src([`${mpBackupFolderPath}/images/${indexName}/*`])
+            .pipe(rename((path) => {
+                console.log('==========rename path', path)
+            }))
+            .pipe(
+                gulp.dest(`${mpFolderPath}/images/${indexName}/`)
+            )
+    }
+    return gulp.series([handleClean, gulp.parallel([handleFile, handleImage])])
+}
+gulp.task('backup2Mp', backup2Mp())
 gulp.task('mp2Backup', mp2Backup())
