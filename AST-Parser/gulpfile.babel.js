d9fc2d6dbb888792ab608d77b699195ef7063fc5
Misaka <misaka1033@gmail.com>
2022-05-06 16:28:52 +0800

    u

--- AST-Parser/gulpfile.babel.js
+++ AST-Parser/gulpfile.babel.js
@@ -1,2 +1,2 @@
-// export { rpWx } from "./lib/replace-wxml-reg"
-export { rpWx } from "./lib/replace-wxml-cheerio"
+export { rpWx } from "./lib/replace-wxml-reg"
+// export { rpWx } from "./lib/replace-wxml-cheerio"
