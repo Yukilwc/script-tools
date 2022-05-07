f62b2a19dde8489d712cfc8ddf460642716b8bc5
Misaka <misaka1033@gmail.com>
2022-05-07 16:35:47 +0800

    u

--- AST-Parser/lib/insert-title-into-js.js
+++ AST-Parser/lib/insert-title-into-js.js
@@ -4,12 +4,12 @@ var rename = require('gulp-rename')
 import fs from 'fs'
 import path from 'path'
 const prettier = require("prettier");
-import pages from '../result/pages.json'
+import pagesJson from '../result/pages.json'
 import {
     globFilter,
     globListFilter
 } from '../tools/index'
-
+import { cn,en } from '../lang/dictionary';
 import { currentPath, targetPath, extJson, commonExt } from './path';
 // 获取命令行参数
 const getArgOptions = () => {
@@ -18,9 +18,34 @@ const getArgOptions = () => {
     return options
 
 }
-const insertTitleIntoJs = () => {
-    console.log('==========pages',pages)
+const insertTitleIntoJs = (cb) => {
+    let pages = pagesJson.filter(page => page.navigationBarTitleText)
+    let res = getKeyByChFromNavTitle('我的')
+    console.log('==========key',res)
+    cb()
 }
+const getKeyByChFromNavTitle = (ch) => {
+    let cnKeys = Object.keys(cn).filter(key => {
+        if (/^navTitle\..*$/g.test(key)) return true
+        else return false
+    })
+    let findRes = cnKeys.find(key => {
+        if (cn[key] === ch) {
+            return true
+        }
+        else {
+            return false
+        }
+    })
+    if (findRes && en[findRes] && en[findRes] !== findRes) {
+        // console.log('==========存在可替换的中文key', findRes, en[findRes])
+        return findRes
+    }
+    else {
+        return ch
+    }
+}
+
 export {
     insertTitleIntoJs
 }
--- AST-Parser/一键添加全部js文件标题.bat
+++ AST-Parser/一键添加全部js文件标题.bat
@@ -1,2 +1,6 @@
+ECHO start sync language dictionary
+CALL npm run syncDic
+ECHO end sync language dictionary
+
 CALL npm run insertTitleIntoJs
 PAUSE
