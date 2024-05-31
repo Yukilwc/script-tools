2cf3defa9f9db84067a08fb2d000e3bda7370b04
Misaka <misaka1033@gmail.com>
2024-05-31 15:17:14 +0800

    u

--- novel_get/22shuquge/main.py
+++ novel_get/22shuquge/main.py
@@ -20,9 +20,11 @@ def main():
     # get_chapters()
     return
 
+
 current_file_path = __file__
 
 current_dir = os.path.dirname(current_file_path)
+
 
 def getIndex():
     driver = webdriver.Chrome()
@@ -54,62 +56,68 @@ def getIndex():
             title = a_tag_contents[0].text.strip()
             href = chapter["href"]
             chapters.append({
-                "title":title,
-                "href":f"{baseUrl}{href}"
+                "title": title,
+                "href": f"{baseUrl}{href}"
             })
     chapters_json = json.dumps(chapters, ensure_ascii=False, indent=4)
 
     # 将JSON字符串保存到本地文件
-    with open('./chapters.json', 'w', encoding='utf-8') as f:
+    with open(os.path.join(current_dir, 'chapters.json'), 'w', encoding='utf-8') as f:
         f.write(chapters_json)
     driver.quit()
+
 
 def get_chapters():
     chapters = []
     # 打开JSON文件并读取其内容
-    with open(os.path.join(current_dir,'chapters.json'), 'r', encoding='utf-8') as f:
+    with open(os.path.join(current_dir, 'chapters.json'), 'r', encoding='utf-8') as f:
         chapters = json.load(f)
     return chapters
+
 
 def generate_book():
     driver = webdriver.Chrome()
     # 打开文件，如果文件不存在则创建，存在则清空内容 ('w' 模式)
-    with open(f'./${book_name}.txt', 'w') as file:
+    with open(os.path.join(current_dir, f'{book_name}.txt'), 'w', encoding="utf-8") as file:
         chapters = get_chapters()
         num = 0
         for chapter in chapters:
-            bodyText = readOneChapter(driver,chapter_url=chapter["href"])
-            one_text = chapter["title"]+ "\n" + bodyText
+            bodyText = readOneChapter(driver, chapter_url=chapter["href"])
+            one_text = chapter["title"] + "\n" + bodyText
             # 循环数组中的每个字符串
             # 将字符串写入文件，并添加换行符
             file.write(one_text + '\n')
-            num+=1
-            if(num>5):
+            num += 1
+            if (num > 5):
                 break
-            
+
     driver.quit()
 
-def readOneChapter(driver,chapter_url):
+
+def readOneChapter(driver, chapter_url):
     page_text = ""
     for i in range(5):
         driver.get(chapter_url)
         html_content = driver.page_source
         soup = BeautifulSoup(html_content, 'html.parser')
         remove_el = soup.select("#nr #nr1 #center_tip")
-        if len(remove_el)>0:
+        if len(remove_el) > 0:
             for r in remove_el:
                 r.decompose()
         book = soup.select("#nr #nr1")
-        text = book[0].text.strip()
-        page_text = page_text + "\n" + text
-        next_el = soup.find(id="pt_next")
-        next_text = next_el.text.strip()
-        if(next_text=="下一页"):
-            chapter_url = baseUrl + next_el["href"]
+        if(len(book)>0):
+            text = book[0].text.strip()
+            page_text = page_text + "\n" + text
+            next_el = soup.find(id="pt_next")
+            next_text = next_el.text.strip()
+            if (next_text == "下一页"):
+                chapter_url = baseUrl + next_el["href"]
+            else:
+                break
         else:
-            break;
-    return page_text 
- 
+            print("当前页面未找到目标元素")
+            print(html_content)
+    return page_text
+
 
 main()
-
