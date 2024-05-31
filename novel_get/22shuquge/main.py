24c5b0b05019586fc8ca52cded72050ad9f681e3
Misaka <misaka1033@gmail.com>
2024-05-31 09:07:18 +0800

    u

--- novel_get/22shuquge/main.py
+++ novel_get/22shuquge/main.py
@@ -1,4 +1,5 @@
 import json
+import os
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -8,14 +9,20 @@ from bs4 import BeautifulSoup
 
 baseUrl = "https://m.22shuquge.com"
 indexUrl = "https://m.22shuquge.com/indexs/8530/"
+book_name = "深海余烬"
 # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 
 def main():
     # getIndex()
-    # chapters = get_chapters()
+    # readOneChapter("https://m.22shuquge.com/b/0/870/698450.html")
+    generate_book()
+    # get_chapters()
     return
 
+current_file_path = __file__
+
+current_dir = os.path.dirname(current_file_path)
 
 def getIndex():
     driver = webdriver.Chrome()
@@ -53,16 +60,56 @@ def getIndex():
     chapters_json = json.dumps(chapters, ensure_ascii=False, indent=4)
 
     # 将JSON字符串保存到本地文件
-    with open('22shuquge_chapters.json', 'w', encoding='utf-8') as f:
+    with open('./chapters.json', 'w', encoding='utf-8') as f:
         f.write(chapters_json)
     driver.quit()
 
 def get_chapters():
     chapters = []
     # 打开JSON文件并读取其内容
-    with open('22shuquge_chapters.json', 'r', encoding='utf-8') as f:
+    with open(os.path.join(current_dir,'chapters.json'), 'r', encoding='utf-8') as f:
         chapters = json.load(f)
     return chapters
+
+def generate_book():
+    driver = webdriver.Chrome()
+    # 打开文件，如果文件不存在则创建，存在则清空内容 ('w' 模式)
+    with open(f'./${book_name}.txt', 'w') as file:
+        chapters = get_chapters()
+        num = 0
+        for chapter in chapters:
+            bodyText = readOneChapter(driver,chapter_url=chapter["href"])
+            one_text = chapter["title"]+ "\n" + bodyText
+            # 循环数组中的每个字符串
+            # 将字符串写入文件，并添加换行符
+            file.write(one_text + '\n')
+            num+=1
+            if(num>5):
+                break
+            
+    driver.quit()
+
+def readOneChapter(driver,chapter_url):
+    page_text = ""
+    for i in range(5):
+        driver.get(chapter_url)
+        html_content = driver.page_source
+        soup = BeautifulSoup(html_content, 'html.parser')
+        remove_el = soup.select("#nr #nr1 #center_tip")
+        if len(remove_el)>0:
+            for r in remove_el:
+                r.decompose()
+        book = soup.select("#nr #nr1")
+        text = book[0].text.strip()
+        page_text = page_text + "\n" + text
+        next_el = soup.find(id="pt_next")
+        next_text = next_el.text.strip()
+        if(next_text=="下一页"):
+            chapter_url = baseUrl + next_el["href"]
+        else:
+            break;
+    return page_text 
+ 
 
 main()
 
